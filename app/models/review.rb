ce7d54bfe04d3e29f3aa06343eeb04cf7ecaf44e
Natasha <natasha.khoe@gmail.com>
2023-01-04 21:43:29 +1100

    commit tree 00f002c83bbd2d036436e23adf78ed579a65a0c7

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ActiveRecord::Base
-  
+# a review belongs to a game
+#create a method to get the game instance for the review using the game foreign id in the the review instance
+    # def game
+    #     #self is the review instance
+    #     Game.find(self.game_id)
+    # end
+#Or you can just put the belongs_to macros to simplify the function
+    belongs_to :game
 end
